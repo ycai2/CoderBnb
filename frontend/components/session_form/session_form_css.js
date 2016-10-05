4afea0b8f15c4a4eb669d04c46089309fd53cf1b
Yisheng Cai <cai.eason@gmail.com>
2016-10-05 14:50:59 -0400

    Fix styling on modal

---
+++ frontend/components/session_form/session_form_css.js
@@ -0,0 +1,25 @@
+const modalStyle = {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(0, 0, 0, 0.75)'
+  },
+  content : {
+    width                      : '450px',
+    position                   : 'absolute',
+    border                     : '1px solid #ccc',
+    background                 : '#fff',
+    overflow                   : 'auto',
+    WebkitOverflowScrolling    : 'touch',
+    borderRadius               : '4px',
+    outline                    : 'none',
+    padding                    : '32px',
+    borderRadius               : '2px',
+    margin                     : 'auto',
+  }
+}
+
+export default modalStyle;
