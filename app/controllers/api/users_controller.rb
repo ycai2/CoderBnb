ef4f633ead5d098d58cdd137b9224b149ed6071b
Yisheng Cai <cai.eason@gmail.com>
2016-10-06 09:14:53 -0400

    Fix signup controller actions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,13 +12,14 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # login
-      redirect_to root_url
+      login(@user)
+      render 'api/users/show'
     else
       render json: @user.errors.full_messages, status: 422
     end
   end
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :first_name, :last_name)
   end
 end
