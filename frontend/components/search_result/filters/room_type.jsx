b02a0da4d72db784311c63ae2f9c7cbeb353948f
Yisheng Cai <cai.eason@gmail.com>
2016-10-08 14:58:37 -0400

    Use flex-box to align

--- frontend/components/search_result/filters/room_type.jsx
+++ frontend/components/search_result/filters/room_type.jsx
@@ -6,15 +6,18 @@ const RoomType = (props) => {
       <span className="room-type-title title">Room type</span>
       <div className="checkbox-control">
         <i className="fa fa-server" aria-hidden="true"></i>
-        <label>Basement<input type="checkbox" id="room-type-basement" value="basement" /></label>
+        <label for="room-type-basement">Basement</label>
+        <input type="checkbox" id="room-type-basement" value="basement" />
       </div>
       <div className="checkbox-control">
         <i className="fa fa-laptop" aria-hidden="true"></i>
-        <label>Lab<input type="checkbox" id="room-type-lab" value="lab" /></label>
+        <label for="room-type-lab">Lab</label>
+        <input type="checkbox" id="room-type-lab" value="lab" />
       </div>
       <div className="checkbox-control">
         <i className="fa fa-rocket" aria-hidden="true"></i>
-        <label>Penthouse<input type="checkbox" id="room-type-penthouse" value="penthouse" /></label>
+        <label for="room-type-penthouse">Penthouse</label>
+        <input type="checkbox" id="room-type-penthouse" value="penthouse" />
       </div>
     </div>
   );
