a5a35df3daa68cdd3f6d69ec716de2f73c461c37
Yisheng Cai <cai.eason@gmail.com>
2016-10-08 15:39:07 -0400

    Fix styling for filters

--- frontend/components/search_result/filters/room_type.jsx
+++ frontend/components/search_result/filters/room_type.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 
 const RoomType = (props) => {
   return (
-    <div className="room-type group">
+    <div className="room-type">
       <span className="room-type-title title">Room type</span>
-      <div className="checkbox-control">
-        <i className="fa fa-server" aria-hidden="true"></i>
-        <label for="room-type-basement">Basement</label>
-        <input type="checkbox" id="room-type-basement" value="basement" />
-      </div>
-      <div className="checkbox-control">
-        <i className="fa fa-laptop" aria-hidden="true"></i>
-        <label for="room-type-lab">Lab</label>
-        <input type="checkbox" id="room-type-lab" value="lab" />
-      </div>
-      <div className="checkbox-control">
-        <i className="fa fa-rocket" aria-hidden="true"></i>
-        <label for="room-type-penthouse">Penthouse</label>
-        <input type="checkbox" id="room-type-penthouse" value="penthouse" />
+      <div className="checkbox-control-group group">
+        <div className="checkbox-control">
+          <i className="fa fa-server" aria-hidden="true"></i>
+          <label htmlFor="room-type-basement">Basement</label>
+          <input type="checkbox" id="room-type-basement" value="basement" />
+        </div>
+        <div className="checkbox-control">
+          <i className="fa fa-laptop" aria-hidden="true"></i>
+          <label htmlFor="room-type-lab">Lab</label>
+          <input type="checkbox" id="room-type-lab" value="lab" />
+        </div>
+        <div className="checkbox-control">
+          <i className="fa fa-rocket" aria-hidden="true"></i>
+          <label htmlFor="room-type-penthouse">Penthouse</label>
+          <input type="checkbox" id="room-type-penthouse" value="penthouse" />
+        </div>
       </div>
     </div>
   );
