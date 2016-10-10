eb6d300923d9623299f932af1c561ca7a60dd44b
Yisheng Cai <cai.eason@gmail.com>
2016-10-10 10:41:55 -0400

    Fix styling on index and add spot form

---
+++ frontend/components/spot_form/spot_form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SpotForm extends React.Component {
+  render() {
+    return (<h1>New Spot</h1>);
+  }
+};
+
+export default SpotForm;
---
+++ frontend/components/spot_form/spot_form_container.jsx
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import SpotForm from './spot_form';
+
+const mapStateToProps = ({  }) => ({
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SpotForm);
