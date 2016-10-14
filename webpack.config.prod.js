aade9b4dec18237f83532f3c4b0d504b9a734bc0
Yisheng Cai <cai.eason@gmail.com>
2016-10-14 11:42:20 -0400

    Make production web pack

---
+++ webpack.config.prod.js
@@ -0,0 +1,42 @@
+var wepack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/coder_bnb.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ],
+    alias: {
+      'react': path.join(__dirname, 'node_modules', 'react')
+    }
+  }
+};
